c494f5c63ec6bd58e0035d2950418b6b20ce46af
arpanspeaks <arpanspeaks@gmail.com>
2020-07-31 02:03:15 +0530

    Add files via upload

---
+++ longestPrefixSuffix.py
@@ -0,0 +1,36 @@
+'''
+Longest Prefix Suffix
+
+https://practice.geeksforgeeks.org/problems/longest-prefix-suffix/0
+
+TC: O(n)
+'''
+
+def computeLPS(s, lps):
+    res = 0
+    prevLps = 0
+    i = 1
+    while i < len(s):
+        if s[i] == s[prevLps]:
+            prevLps += 1
+            lps[i] = prevLps
+            res = max(res, prevLps)
+            i += 1
+        else:
+            if prevLps == 0:
+                lps[i] = 0
+                i += 1
+            else:
+                prevLps = lps[prevLps-1]
+    return res
+    
+def main():
+    t = int(input())
+    while t:
+        s = str(input())
+        lps = [0] * len(s)
+        print(computeLPS(s, lps))
+        t -= 1
+        
+if __name__ == '__main__':
+    main()
